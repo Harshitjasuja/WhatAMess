5729598090c054290d0b2606c073d85812c74b4a
manassrv29 <96646931+manassrv29@users.noreply.github.com>
2025-03-12 20:14:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import SearchBar from "./components/searchbar";
 import MessCard from "./components/messcard";
-import WomenMode from "./components/WomenMode"
+import WomenMode from "./components/WomenMode";
 import VegNonVegToggle from "./components/VegNonveg";
-function App() {
-  return (
-    <div > 
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Cart from "./components/Cart.js"; // Import the Cart component
+import Reward from "./components/Reward"; // Import the Reward component
 
-     
+function Homepage() {
+  return (
+    <div>
       <Navbar />
       <SearchBar />
-      <MessCard/>
-      <WomenMode/>
-      <VegNonVegToggle/>
-      </div>
-    
+      <MessCard />
+      <WomenMode />
+      <VegNonVegToggle />
+    </div>
   );
 }
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/cart" element={<Cart />} /> {/* Add route for Cart Page */}
+        <Route path="/rewards" element={<Reward />} /> {/* Add route for Reward Page */}
+      </Routes>
+    </Router>
+  );
+}
+
+// eslint-disable-next-line semi
 export default App;
